c98f7776701ea9b80aeaf6efc60571fbe3ebc343
PetrB <petr@Petr-MacBook-Pro.local>
2019-03-04 07:38:56 +0100

    Update MainActivity.kt

--- app/src/main/java/cz/budikpet/bachelorwork/MainActivity.kt
+++ app/src/main/java/cz/budikpet/bachelorwork/MainActivity.kt
@@ -11,28 +11,41 @@ import net.openid.appauth.ResponseTypeValues
 import net.openid.appauth.AuthorizationRequest
 import android.content.Intent
 import android.app.PendingIntent
+import android.util.Log
 import net.openid.appauth.AuthorizationService
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MY_MainActivity"
+
     private val clientId = "1932312b-4981-4224-97b1-b45ad041a4b7"
     private val redirectUri = Uri.parse("net.openid.appauthdemo:/oauth2redirect")
     private val scope = "cvut:sirius:personal:read"
-
-    val serviceConfig = AuthorizationServiceConfiguration(
-        Uri.parse("https://auth.fit.cvut.cz/oauth/authorize"), // authorization endpoint
-        Uri.parse("https://auth.fit.cvut.cz/oauth/token") // token endpoint
-    )
 
     private val authStateManager: AuthStateManager by lazy { AuthStateManager(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (authStateManager.authState!!.isAuthorized()) {
+            Log.i(TAG, "User is already authenticated, proceeding to token activity")
+            startActivity(Intent(this, AppAuthTest::class.java))
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_main)
 
-        authStateManager.authState = AuthState(serviceConfig)
+        if(authStateManager.authState!!.authorizationServiceConfiguration == null) {
+            Log.i(TAG, "auth config needs to be established")
+            val serviceConfig = AuthorizationServiceConfiguration(
+                Uri.parse("https://auth.fit.cvut.cz/oauth/authorize"), // authorization endpoint
+                Uri.parse("https://auth.fit.cvut.cz/oauth/token") // token endpoint
+            )
+            authStateManager.authState = AuthState(serviceConfig)
+        }
 
         val authRequestBuilder = AuthorizationRequest.Builder(
-            serviceConfig, // the authorization service configuration
+            authStateManager.authState!!.authorizationServiceConfiguration!!, // the authorization service configuration
             clientId, // the client ID, typically pre-registered and static
             ResponseTypeValues.CODE, // the response_type value: we want a code
             redirectUri // the redirect URI to which the auth response is sent
